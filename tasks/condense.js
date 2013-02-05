1792d408b1081a9fb859aa7c5f2dd91c01fdf873
Erich Healy <ErichRHealy@gmail.com>
2013-02-04 21:09:56 -0800

    Initial commit

---
+++ tasks/condense.js
@@ -0,0 +1,54 @@
+
+
+module.exports = function(grunt){
+  'use strict';
+
+  var path = require('path');
+  var _ = grunt.util._;
+
+  grunt.registerMultiTask('condense', 'Condense your CSS', function() {
+
+    // Get some defaults for our options
+    var options = this.options({
+      separator: grunt.util.linefeed
+    });
+
+    // Write out the options that we got if we got any options
+    grunt.verbose.writeflags(options, 'Options');
+
+    this.files.forEach(function(file) {
+      var output = file.src.filter(function(filepath) {
+
+        // Warn on and remove invalid source files (if nonull was set).
+        if (!grunt.file.exists(filepath)) {
+
+          grunt.log.warn('Source file "' + filepath + '" not found.');
+          return false;
+        } else {
+          return true;
+        }
+      }).map(function(filepath) {
+
+        // Actually condense a file
+        return condense(filepath, options);
+
+      // Normalize CRLF
+      }).join(grunt.util.normalizelf(options.separator));
+
+      // debugger;
+
+      if (output.length < 1) {
+        grunt.log.warn('Destination not written because compiled files were empty.');
+      } else {
+        grunt.file.write(file.dest, output);
+        grunt.log.writeln('File ' + file.dest + ' created.');
+      }
+    });
+  });
+
+  var condense = function(file, options) {
+    grunt.log.write('Condensing');
+    grunt.log.writeln(arguments);
+  };
+
+};
